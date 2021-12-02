39c86a7f385ea13e28324c7608cf1c501d645fdf
TeamDman <TeamDman9201@gmail.com>
2021-12-02 22:16:01 +0000

    Add mel spectro and display func

--- data/VCTK.py
+++ data/VCTK.py
@@ -7,6 +7,10 @@ import numpy as np
 class VCTKDataset(torch.utils.data.Dataset):
     def __init__(self, path) -> None:
         super().__init__()
+        self.sample_rate=16000
+        self.win_length=800
+        self.hop_length=200
+        self.n_mels=80
         
         # load file paths
         text_file_paths = sorted(glob.glob(os.path.join(path, "txt", "*", "*.txt")))
@@ -30,18 +34,33 @@ class VCTKDataset(torch.utils.data.Dataset):
         audio, sr = librosa.load(audio)
 
         # convert from 48khz to 16khz for efficiency
-        audio = librosa.resample(audio, orig_sr=sr, target_sr=16000)
+        audio = librosa.resample(audio, orig_sr=sr, target_sr=self.sample_rate)
 
         # convert to spectrogram
         # https://librosa.org/doc/main/generated/librosa.feature.melspectrogram.html
         spectro = librosa.feature.melspectrogram(
             y=audio,
-            sr=16000,
-            win_length=800,
-            hop_length=200
+            sr=self.sample_rate,
+            win_length=self.win_length,
+            hop_length=self.hop_length,
+            n_mels=self.n_mels,
         )
         return text, spectro
         # return text, audio
 
     def __len__(self):
         return len(self.data)
+
+    def show(self, entry):
+        import matplotlib.pyplot as plt
+        text, mel_spectro = entry
+        librosa.display.specshow(
+            librosa.power_to_db(mel_spectro, ref=np.max),
+            sr=self.sample_rate,
+            hop_length=self.hop_length,
+            x_axis="time",
+            y_axis="mel"
+        )
+        plt.colorbar(format="%+2.0f dB")
+
+
