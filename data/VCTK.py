bfc06e51639f48a999ca7931fc13c17b604f07e7
TeamDman <TeamDman9201@gmail.com>
2021-12-02 21:38:20 +0000

    add transcript reading and audio downsampling to data loader

--- data/VCTK.py
+++ data/VCTK.py
@@ -2,8 +2,7 @@ import torch
 import os
 import glob
 import librosa
-
-import tqdm.notebook as tqdm
+import numpy as np
 
 class VCTKDataset(torch.utils.data.Dataset):
     def __init__(self, path) -> None:
@@ -14,19 +13,29 @@ class VCTKDataset(torch.utils.data.Dataset):
         audio_file_paths = sorted(glob.glob(os.path.join(path, "wav48", "*", "*.wav")))
         self.data = []
 
-        for text, audio in tqdm.tqdm(list(zip(text_file_paths, audio_file_paths))):
+        for text, audio in zip(text_file_paths, audio_file_paths):
             # sanity check
             assert os.path.basename(os.path.splitext(text)[0]) == os.path.basename(os.path.splitext(audio)[0])
-
 
             self.data += [(text, audio)]
 
     def __getitem__(self, index):
         text, audio = self.data[index]
-        audio = librosa.load(audio)
-        audio = librosa.resample(audio,)
+        
+        # read transcript
+        with open(text, "r") as handle:
+            text = "".join([line for line in handle.readlines()]).strip()
+
+        # load audio, also get sample rate
+        audio, sr = librosa.load(audio)
+
+        # convert from 48khz to 16khz for efficiency
+        audio = librosa.resample(audio, orig_sr=sr, target_sr=16000)
+
+        # convert to spectrogram
         spectro = librosa.feature.melspectrogram()
         return text, spectro
+        # return text, audio
 
     def __len__(self):
         return len(self.data)
