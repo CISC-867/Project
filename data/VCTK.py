5c70f6c6eba735f7d0c6befc98fb5e407c53929d
TeamDman <TeamDman9201@gmail.com>
2021-12-02 03:12:50 +0000

    Add dataset class

---
+++ data/VCTK.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from glob import glob
+from os.path import join, basename, splitext
+
+class VCTKDataset(Dataset):
+    def __init__(self, path) -> None:
+        super().__init__()
+        
+        # load file paths
+        text_file_paths = sorted(glob(join(path, "txt", "*", "*.txt")))
+        audio_file_paths = sorted(glob(join(path, "wav48", "*", "*.txt")))
+        
+        self.data = []
+
+        for text, audio in zip(text_file_paths, audio_file_paths):
+            # sanity check
+            assert basename(splitext(text)[0]) == basename(splitext(audio)[0])
+
+            self.data += (text, audio)
+
+    def __getitem__(self, index):
+        return self.data[index]
