fc8ccec6b8b6bfa982dc5a390ae3ab743befadb6
TeamDman <TeamDman9201@gmail.com>
2021-12-02 03:34:42 +0000

    Add quick setup script for dom

---
+++ setup-dom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "TeamDman"
+git config --global user.email "TeamDman9201@gmail.com"
+
+code --install-extension "k--kato.intellij-idea-keybindings"
+code --install-extension "ms-python.python"
