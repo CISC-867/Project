8d76eebfe6dbd88275cb3f3c064ab85f908fe03d
TeamDman <TeamDman9201@gmail.com>
2021-12-03 00:11:58 +0000

    remove download shit

--- data/VCTK.py
+++ data/VCTK.py
@@ -4,50 +4,6 @@ import glob
 import librosa
 import numpy as np
 
-def download_dataset(version, save_path):
-    if version == "Full":
-        url = "https://queensuca-my.sharepoint.com/:u:/g/personal/16drp4_queensu_ca/EcnCAgHdAwBJmWk9vNcpiKgB5Rhi6GNtSchx6zGEhWe-fw?download=1"
-        filename = "VCTK-Corpus.tar.gz"
-    elif version == "Small":
-        url = "https://queensuca-my.sharepoint.com/:u:/g/personal/16drp4_queensu_ca/Ebz_WoXK4T9KjbJq_SFCsZQB94PqsKOnQ9HwB5olpGeQIw?download=1"
-        filename = "VCTK-Corpus-small.7z"
-    elif version == "Smaller":
-        url = "https://queensuca-my.sharepoint.com/:u:/g/personal/16drp4_queensu_ca/EfRSVbO-ohRFlUFhA9lx4qsBVl4s5FU676hF9uTRSAjOgg?download=1"
-        filename = "VCTK-Corpus-smaller.tar.gz"
-    
-    archive_path = os.path.join(save_path, filename)
-    base, ext = os.path.splitext(os.path.basename(archive_path))
-    destination = os.path.join(os.path.dirname(archive_path), base)
-    if os.path.exists(destination):
-        print("already extracted")
-        return
-
-    # import urllib.request
-    # import requests.utils
-    # print("got",url)
-    # url = requests.utils.requote_uri(url)
-    import requests
-    if not os.path.exists(archive_path):
-        print(f"downloading archive")
-        with open(archive_path, "wb") as out:
-            req = requests.get(url)
-            out.write(req.content)
-    else:
-        print("archive already downloaded")
-        # urllib.request.urlretrieve(url, archive_path)
-
-
-    print("extracting archive")
-    import shutil
-    shutil.unpack_archive(archive_path, destination)
-    # import tarfile
-    # if ext == ".tar.gz":
-    #     print("tar.gz")
-    # elif ext == ".tar":
-    #     print("tar")
-    # elif ext == ".7z":
-    #     print("7z")
-
 class VCTKDataset(torch.utils.data.Dataset):
     def __init__(self, path) -> None:
         super().__init__()
