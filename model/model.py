a7f2ff734bfbcc56881c965c8a0235c84e4a5d9a
TeamDman <TeamDman9201@gmail.com>
2021-12-03 17:30:05 +0000

    begin trying to get vocoder working

--- model/model.py
+++ model/model.py
@@ -2,6 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import sys
+import os.path
+
+sys.path.append(os.path.abspath("ForwardTacotron"))
+from models.fatchord_version import WaveRNN
+
+
 class MyModel(nn.Module):
     def __init__(self) -> None:
         super(MyModel, self).__init__()
@@ -12,7 +19,7 @@ class MyModel(nn.Module):
         self.batch2 = nn.BatchNorm1d(num_features=512)
         self.conv3 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5)
         self.batch3 = nn.BatchNorm1d(num_features=512)
-
+        
         self.lstm1 = nn.LSTM(
             input_size=512,
             hidden_size=32,
@@ -29,11 +36,11 @@ class MyModel(nn.Module):
             batch_first=True
         )
 
-        self.conv4 = nn.Conv1d(in_channels=1024, out_channels=512, kernel_size=5)
+        self.conv4 = nn.Conv1d(in_channels=1024, out_channels=512, kernel_size=5, padding="same")
         self.batch4 = nn.BatchNorm1d(num_features=512)
-        self.conv5 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5)
+        self.conv5 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5, padding="same")
         self.batch5 = nn.BatchNorm1d(num_features=512)
-        self.conv6 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5)
+        self.conv6 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5, padding="same")
         self.batch6 = nn.BatchNorm1d(num_features=512)
 
         self.lstm3 = nn.LSTM(
@@ -48,6 +55,8 @@ class MyModel(nn.Module):
             in_features=2048,
             out_features=80
         )
+
+        self.vocoder = WaveRNN()
 
     def forward(self, mel_spectro):
         x = mel_spectro
