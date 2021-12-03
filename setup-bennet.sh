b7fc807f4fa4b0d36c65593469222a7baef72f8a
BennetMontgomery <bennetlogan@gmail.com>
2021-12-03 17:41:29 +0000

    Added setup script for Bennet

---
+++ setup-bennet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "BennetMontgomery"
+git config --global user.email "bennetlogan@gmail.com"
+
+code --install-extension "k--kato.intellij-idea-keybindings"
+code --install-extension "ms-python.python"
