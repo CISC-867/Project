2ce1100d7220968328a02293d47638757769f621
TeamDman <TeamDman9201@gmail.com>
2021-12-03 02:44:38 +0000

    more lstm work

--- model/model.py
+++ model/model.py
@@ -13,16 +13,31 @@ class MyModel(nn.Module):
         self.conv3 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5)
         self.batch3 = nn.BatchNorm1d(num_features=512)
 
-        self.lstm = nn.LSTM(input_size=512, hidden_size=32, bidirectional=True, batch_first=True)
+        self.lstm_hidden_size=32
+        self.lstm1 = nn.LSTM(input_size=512, hidden_size=self.lstm_hidden_size, bidirectional=True, batch_first=True)
+        self.lstm2 = nn.LSTM(input_size=512, hidden_size=self.lstm_hidden_size, bidirectional=True, batch_first=True)
 
     def forward(self, mel_spectro):
         x = mel_spectro
-        print(x.shape)
+        print(x.shape, end="\n\n")
+
         x = self.batch1(F.relu(self.conv1(x)))
-        print(x.shape)
+        print(x.shape, end="\n\n")
+
         x = self.batch2(F.relu(self.conv2(x)))
-        print(x.shape)
+        print(x.shape, end="\n\n")
+
         x = self.batch3(F.relu(self.conv3(x)))
+        print(x.shape, end="\n\n")
+
+        x = x.transpose(2,1)
+        print(x.shape, end="\n\n")
+
+        x, _ = self.lstm1(x)
         print(x.shape)
-        x = self.lstm(x)
-        return x
+        x_forward = x[:, :, :self.lstm_hidden_size]
+        print(x_forward.shape)
+        x_backward = x[:,:, self.lstm_hidden_size:]
+        print(x_backward.shape, end="\n\n")
+        # x, _ = self.lstm2(x)
+        return x, x_forward, x_backward
