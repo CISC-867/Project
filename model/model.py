a36d44951ef5d56ab4ed721db3f31a640ced1a78
TeamDman <TeamDman9201@gmail.com>
2021-12-04 01:14:04 +0000

    add vocoder to training

--- model/model.py
+++ model/model.py
@@ -2,11 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import sys
-import os.path
-
-sys.path.append(os.path.abspath("ForwardTacotron"))
-from models.fatchord_version import WaveRNN
 
 
 class SpectrogramModel(nn.Module):
@@ -77,30 +72,3 @@ class SpectrogramModel(nn.Module):
         x, _ = self.lstm3(x)
         x = self.dense(x)
         return x
-
-class FullModel(nn.Module):
-    def __init__(self) -> None:
-        super(FullModel, self).__init__()
-        self.spectro = SpectrogramModel()
-
-        self.vocoder = WaveRNN(
-            rnn_dims=512,
-            fc_dims=512,
-            bits=9, # OrigAuthor: bit depth of signal
-            pad=2, # OrigAuthor: this will pad the input so that the resnet can 'see' wider than input length
-            upsample_factors=(5, 5, 8), # OrigAuthor: NB - this needs to correctly factorise hop_length
-            feat_dims=80,
-            compute_dims=128,
-            res_out_dims=128,
-            res_blocks=10,
-            hop_length=200,
-            sample_rate=16000,
-            mode="RAW", # OrigAuthor: either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
-        )
-
-    def forward(self, clips, spectros):
-        spectros = self.spectro(spectros)
-        spectros = spectros.transpose(2,1)
-        print(clips.shape, spectros.shape)
-        y_pred = self.vocoder(clips, spectros)
-        return y_pred
--- model/trainer.py
+++ model/trainer.py
@@ -1,27 +1,46 @@
 import torch
 import torch.optim
 import torch.nn as nn
-
+import torch.nn.functional as F
 import tqdm.notebook as tqdm
 
 class Trainer:
-    def __init__(self, model, device) -> None:
+    def __init__(self, model, vocoder, device) -> None:
         self.model = model
+        self.vocoder = vocoder
         self.device = device
-        self.optimizer = torch.optim.Adam(model.parameters(), lr=0.001)  
-        self.criterion = nn.L1Loss()
+        self.model_optimizer = torch.optim.Adam(model.parameters(), lr=0.001)  
+        self.vocoder_optimizer = torch.optim.Adam(vocoder.parameters(), lr=0.001)  
+        self.spectro_loss_func = nn.L1Loss()
+        self.vocoder_loss_func = F.cross_entropy
 
     def train_step(self, x, y):
-        y_pred = self.model(x)
-        y_pred = y_pred.transpose(2,1)
+        y_pred_spectro = self.model(x)
+        y_pred_spectro = y_pred_spectro.transpose(2,1)
+        spectro_loss = self.spectro_loss_func(y_pred_spectro, x)
+        
+        # for some reason our dataset returns 129 instead of 128 time slices
+        # lets just chop the last one off for now.
         x = x[:,:,:128]
+        
+        y_pred_wav = self.vocoder(y_pred_spectro, None)
+        vocoder_loss = self.vocoder_loss_func(y_pred_wav)
 
-        loss = self.criterion(y_pred, x)
-        print(loss)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        del y_pred
+        total_loss = spectro_loss + vocoder_loss
+
+        print(total_loss, spectro_loss, vocoder_loss)
+
+        self.model_optimizer.zero_grad()
+        self.vocoder_optimizer.zero_grad()
+        total_loss.backward()
+        self.model_optimizer.step()
+        self.vocoder_optimizer.step()
+
+        ## free tensor from gpu memory?
+        ## not sure if this works
+        # del y_pred_spectro
+
+        return total_loss
 
 
     def train(self, model, dataset, device):
