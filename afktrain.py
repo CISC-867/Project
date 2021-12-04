e1bcd22d9a61be6c9ca3967fd422afc3b19ae0b7
TeamDman <TeamDman9201@gmail.com>
2021-12-04 09:52:37 +0000

    update afktrain

--- afktrain.py
+++ afktrain.py
@@ -1,8 +1,19 @@
 import model.trainer
+import torch
 
 if __name__ == "__main__":
     trainer = model.trainer.Trainer(
-        checkpoint=5,
-        load_from_checkpoint=False
+        device=torch.device("cuda"),
+        checkpoint=7,
+        load_from_checkpoint=True,
     )
-    trainer.train(save_every_n=100)
+    try:
+        trainer.train(
+            epochs=500,
+            save_every_n=50,
+            batch_size=4,
+            run_name="runs/overnight1",
+        )
+    except Exception:
+        trainer.checkpoint=trainer.checkpoint+"failure"
+        trainer.save()
